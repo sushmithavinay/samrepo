62fb2cd8dcc7109d152e1899e3c78664b4a26b15
sushmithavinay <113432119+sushmithavinay@users.noreply.github.com>
2022-11-15 15:11:07 +0530

    Update app_naapi_automation.py

--- LAMBDACODE/app_naapi_automation.py
+++ LAMBDACODE/app_naapi_automation.py
@@ -1,8 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
+    """Sample pure Lambda function"""
     return {
         "statusCode": 200,
         "body": json.dumps({
